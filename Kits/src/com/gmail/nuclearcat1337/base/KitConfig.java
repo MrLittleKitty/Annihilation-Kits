ab0fe40c2a8f401dd9baad1d483126428b0e1ea1
Eric <nuclearcat1337@gmail.com>
2015-02-06 18:51:13 -0800

    Removed all database handling for kits in preparation for separate plugin

--- Kits/src/com/gmail/nuclearcat1337/base/KitConfig.java
+++ Kits/src/com/gmail/nuclearcat1337/base/KitConfig.java
@@ -2,40 +2,17 @@ package com.gmail.nuclearcat1337.base;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 
-import com.gmail.nuclearcat1337.anniPro.anniGame.AnniPlayer;
-import com.gmail.nuclearcat1337.anniPro.itemMenus.MenuItem;
-import com.gmail.nuclearcat1337.anniPro.kits.Kit;
-import com.gmail.nuclearcat1337.anniPro.main.AnniArgument;
-import com.gmail.nuclearcat1337.anniPro.main.AnniCommand;
 import com.gmail.nuclearcat1337.anniPro.main.AnnihilationMain;
-import com.gmail.nuclearcat1337.anniPro.utils.IDTools;
-import com.gmail.nuclearcat1337.anniPro.xp.AsyncLogQuery;
-import com.gmail.nuclearcat1337.anniPro.xp.AsyncQuery;
-import com.gmail.nuclearcat1337.anniPro.xp.Database;
-import com.gmail.nuclearcat1337.anniPro.xp.XPSystem;
-import com.google.common.base.Predicate;
 
 public class KitConfig
 {
@@ -50,7 +27,7 @@ public class KitConfig
 	private YamlConfiguration kitConfig;
 	private boolean useAllKits;
 	private boolean useDatabase;
-	private Database database;
+	//private Database database;
 	//private final ChatColor aqua = ChatColor.AQUA;
 	private KitConfig()
 	{
@@ -71,112 +48,8 @@ public class KitConfig
 			ConfigurationSection section = kitConfig.getConfigurationSection("StarterKits");
 			useAllKits = section.getBoolean("EnableAllKits");
 			useDatabase = section.getBoolean("UseDatabase");
-			if(useDatabase)
-			{
-				database = XPSystem.getDatabase();
-				if(database != null)
-				{
-					database.updateSQL("CREATE TABLE IF NOT EXISTS tbl_player_kits (ID VARCHAR(40), Kit VARCHAR(20))");
-					for(AnniPlayer p : AnniPlayer.getPlayers())
-					{
-						loadKits(p);
-					}
-					Bukkit.getPluginManager().registerEvents(new Listener(){
-						@EventHandler(priority=EventPriority.MONITOR)
-						public void kitLoader(PlayerJoinEvent event)
-						{
-							AnniPlayer p = AnniPlayer.getPlayer(event.getPlayer().getUniqueId());
-							if(p != null)
-								loadKits(p);
-						}
-						
-						@EventHandler(priority=EventPriority.MONITOR)
-						public void kitLoader(PlayerTeleportEvent event)
-						{
-							AnniPlayer p = AnniPlayer.getPlayer(event.getPlayer().getUniqueId());
-							if(p != null)
-								loadKits(p);
-						}
-					}, AnnihilationMain.getInstance());
-					
-					//Bukkit.getLogger().info("[Annihilation] Registering the Argument");
-					AnniCommand.registerArgument(new AnniArgument(){
-						@Override
-						public void executeCommand(final CommandSender sender, String label, final String[] args)
-						{
-							if(args != null && args.length > 2 && useDatabase)
-							{
-								IDTools.getUUID(args[2], new Predicate<UUID>(){
-									@Override
-									public boolean apply(UUID id)
-									{
-										if(id != null)
-										{
-											Kit kit = Kit.getKit(args[1]);
-											if(kit != null)
-											{
-												if(args[0].equalsIgnoreCase("add"))
-												{
-													sender.sendMessage(ChatColor.GREEN+"Kit added.");
-													addKit(kit.getName(), id);
-												}
-												else if(args[0].equalsIgnoreCase("remove"))
-												{
-													sender.sendMessage(ChatColor.RED+"Kit removed.");
-													removeKit(kit.getName(), id);
-												}
-												else 
-													sender.sendMessage(ChatColor.RED+"Operation "+ChatColor.GOLD+args[0]+ChatColor.RED+" is not supported.");
-											}
-											else 
-												sender.sendMessage(ChatColor.RED+"Could not locate the kit you specified.");
-										}
-										else 
-											sender.sendMessage(ChatColor.RED+"Could not locate the player you specified.");
-										return false;
-									}});			
-							}
-						}
-	
-						@Override
-						public String getArgumentName()
-						{
-							return "Kit";
-						}
-	
-						@Override
-						public String getHelp()
-						{			
-							return ChatColor.LIGHT_PURPLE+"Kit [add,remove] <kit> <player>--"+ChatColor.GREEN+"adds or removes a kit from a player.";
-						}
-	
-						@Override
-						public MenuItem getMenuItem()
-						{
-							return null;
-						}
-	
-						@Override
-						public String getPermission()
-						{
-							return null;
-						}
-	
-						@Override
-						public boolean useByPlayerOnly()
-						{
-							return false;
-						}});
-				}
-				else 
-					useDatabase = false;
-			}
-//			else
-//			{
-//				//this means we need to register permissions for each of the kits
-//			}
 		}
-		catch (IOException | ClassNotFoundException | SQLException e)
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
@@ -292,104 +165,6 @@ public class KitConfig
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void removeKit(final String kit, final UUID player)
-	{
-		AnniPlayer p = AnniPlayer.getPlayer(player);
-		if(p != null)
-		{
-			Object obj = p.getData("Kits");
-			if(obj != null && obj instanceof List)
-			{
-				List<String> str = (List<String>)obj;
-				str.remove(kit.toLowerCase());
-			}
-		}
-		database.addNewAsyncLogQuery(new AsyncLogQuery(){
-			@Override
-			public String getQuery()
-			{
-				//return "INSERT INTO tbl_player_kits (ID,Kit) VALUES ('"+player.toString()+"','"+kit+"');";
-				return "DELETE FROM tbl_player_kits WHERE ID='"+player+"' AND Kit='"+kit+"';";
-			}});
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void addKit(final String kit, final UUID player)
-	{
-		AnniPlayer p = AnniPlayer.getPlayer(player);
-		if(p != null)
-		{
-			Object obj = p.getData("Kits");
-			if(obj != null && obj instanceof List)
-			{
-				((List<String>)obj).add(kit.toLowerCase());
-			}
-		}
-		database.addNewAsyncLogQuery(new AsyncLogQuery(){
-			@Override
-			public String getQuery()
-			{
-				return "INSERT INTO tbl_player_kits (ID,Kit) VALUES ('"+player.toString()+"','"+kit+"');";
-			}});
-	}
-	
-	public void loadKits(final AnniPlayer player)
-	{
-		Object o = player.getData("KitsLoaded");
-		if(o == null || (boolean)o == false)
-		{
-			database.addNewAsyncQuery(new KitLoader(player));
-			player.setData("KitsLoaded", true);
-		}
-	}
-	
-	private class KitLoader implements AsyncQuery
-	{
-		private final AnniPlayer player;
-		private List<String> kits;
-		public KitLoader(AnniPlayer p)
-		{
-			this.player = p;
-		}
-		
-		
-		@Override
-		public void run()
-		{
-			player.setData("Kits", kits);
-		}
-
-		@Override
-		public String getQuerey() 
-		{	
-			return "SELECT * FROM tbl_player_kits WHERE ID='"+player.getID()+"';";
-		}
-
-		@Override
-		public boolean isCallback() 
-		{
-			return true;
-		}
-
-		@Override
-		public void setResult(ResultSet result) 
-		{
-			kits = new ArrayList<String>();
-			try
-			{
-				while(result.next())
-				{
-					kits.add(result.getString("Kit").toLowerCase());
-				}
-			}
-			catch(SQLException e)
-			{
-				
-			}
 		}
 	}
 	
